4348fa48cfb9f9db45482190494236f43ef7f07e
Sebastian Muszytowski <sebastian@muszytowski.net>
2015-04-11 15:16:41 +0200

    zeugs

--- mode_s.py
+++ mode_s.py
@@ -1,5 +1,6 @@
 from ctypes import *
 from rtlsdr import *
+from time import gmtime
 from libmodes import *
 from pprint import pprint
 #from bitstring import *
@@ -21,7 +22,7 @@ class ModeSDetectorMessage():
 	timestampMsg	= None
 	remote 			= None
 	signalLevel 	= None
-	ca 		= None
+	capability 		= None
 	iid 	= None
 	metype 	= None
 	mesub 	= None
@@ -36,17 +37,49 @@ class ModeSDetectorMessage():
 	vert_rate 	= None
 	velocity 	= None
 	fs 			= None
+	modeA 		= None
 	altitude 	= None
 	unit 	= None
 	bFlags 	= None
 
+	"""
+	Initializes the object with a mode_s message struct
+	TODO: support for bflags
+	"""
 	def __init__(self, modesMessage):
-		self.msg = "".join("{:02x}".format(ord(c)) for c in modesMessage.msg)
-		self.addr = "{:06x}".format(modesMessage.addr)
-		self.altitude = modesMessage.altitude
-		self.unit = 'feet' if modesMessage.unit == 0 else 'meter'
+		self.msg 		= "".join("{:02x}".format(ord(c)) for c in modesMessage.msg)
+		self.msgbits 	= modesMessage.msgbits
+		self.msgtype 	= modesMessage.msgtype
+		self.crcok 		= False if modesMessage == 0 else True
+		self.crc 		= "{:06x}".format(modesMessage.crc)
+		self.correctedbits = modesMessage.correctedbits
+		self.corrected 	= modesMessage.corrected
+		self.addr 		= "{:06x}".format(modesMessage.addr)
+		self.phase_corrected = False if modesMessage.phase_corrected == 0 else True
+		# note: this timestamp is left out at the moment
+		self.timestampMsg 	= gmtime()
+		self.remote 		= modesMessage.remote
+		self.signalLevel	= ord(modesMessage.signalLevel)
+		self.capability		= modesMessage.ca	
+		self.iid			= modesMessage.iid
+		self.metype			= modesMessage.metype
+		self.mesub			= modesMessage.mesub
+		self.heading		= modesMessage.heading
+		self.raw_latitude	= modesMessage.raw_latitude
+		self.raw_longitude	= modesMessage.raw_longitude
+		self.fLat			= modesMessage.fLat
+		self.fLon			= modesMessage.fLon
+		self.flight			= modesMessage.flight
+		self.ew_velocity	= modesMessage.ew_velocity
+		self.ns_velocity	= modesMessage.ns_velocity
+		self.vert_rate		= modesMessage.vert_rate
+		self.velocity 		= modesMessage.velocity
+		self.fs 			= modesMessage.fs
+		self.modeA 			= modesMessage.modeA
+		self.altitude 	= modesMessage.altitude
+		self.unit 		= 'feet' if modesMessage.unit == 0 else 'meter'
+		self.bFlags		= modesMessage.bFlags
 
-		print self.addr
 
 
 class ModeSDetector(object):
@@ -63,7 +96,7 @@ class ModeSDetector(object):
 		self.rtlsdr.set_gain(100)
 		"""
 
-		f = open('/tmp/output.bin', 'rb')
+		f = open('output.bin', 'rb')
 		p = create_string_buffer(f.read()) 
 		print p
 
@@ -73,7 +106,7 @@ class ModeSDetector(object):
 		mm = libModeS.processData(p)
 		#pprint(mm.contents.unit)
 		foo = ModeSDetectorMessage(mm.contents)
-		pprint(foo)
+		pprint(vars(foo))
 		#print self.foo.contents
 
 modes = ModeSDetector()
